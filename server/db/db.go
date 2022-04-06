798803186d1a375b7d26d6a79604293c230f4688
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-04-06 22:08:16 +0900

    feat: 🎸 DB connect

--- server/db/db.go
+++ server/db/db.go
@@ -1,23 +1,39 @@
 package db
 
-import "github.com/jinzhu/gorm"
+import (
+	"database/sql"
 
-var (
-	DB *gorm.DB
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func DBConnect() *gorm.DB {
-	DBMS := "mysql"
-	USER := "root"
-	PASS := "root"
-	PROTOCOL := "tcp(db:3306)"
-	DBNAME := "oms"
-	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?parseTime=true"
-
-	db, err := gorm.Open(DBMS, CONNECT)
+// DB接続する関数
+// sqlboiler
+// *sql.DBをreturnする
+func DBConnect() *sql.DB {
+	db, err := sql.Open("mysql", "root:root@tcp(db:3306)/oms?parseTime=true")
 	if err != nil {
 		panic(err.Error())
 	}
 
 	return db
 }
+
+// var (
+// 	DB *gorm.DB
+// )
+
+// func DBConnect() *gorm.DB {
+// 	DBMS := "mysql"
+// 	USER := "root"
+// 	PASS := "root"
+// 	PROTOCOL := "tcp(db:3306)"
+// 	DBNAME := "oms"
+// 	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?parseTime=true"
+
+// 	db, err := gorm.Open(DBMS, CONNECT)
+// 	if err != nil {
+// 		panic(err.Error())
+// 	}
+
+// 	return db
+// }
--- server/main.go
+++ server/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/lil-shimon/oms/db"
 	// _ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
-	// db.DBConnect()
+	db.DBConnect()
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
